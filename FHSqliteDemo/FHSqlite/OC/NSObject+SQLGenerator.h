8e40fb47e31a79aead4a7619d13469e55e595733
godL <547188371@qq.com>
2017-06-08 22:14:03 +0800

    delete SQLGenerator.c and add NSObject+SQLGenerator

---
+++ FHSqliteDemo/FHSqlite/OC/NSObject+SQLGenerator.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+SQLGenerator.h
+//  FHSqliteDemo
+//
+//  Created by 李浩 on 2017/6/8.
+//  Copyright © 2017年 FromHell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (SQLGenerator)
+
+@end
---
+++ FHSqliteDemo/FHSqlite/OC/NSObject+SQLGenerator.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+SQLGenerator.m
+//  FHSqliteDemo
+//
+//  Created by 李浩 on 2017/6/8.
+//  Copyright © 2017年 FromHell. All rights reserved.
+//
+
+#import "NSObject+SQLGenerator.h"
+
+@implementation NSObject (SQLGenerator)
+
+@end
