40d34c5f1f8d0c9e44623cc117087f85c298cf9c
godL <547188371@qq.com>
2017-06-20 22:59:54 +0800

    add lrucache

---
+++ FHSqliteDemo/FHSqlite/Core/lru_cache.c
@@ -0,0 +1,9 @@
+//
+//  lru_cache.c
+//  FHSqliteDemo
+//
+//  Created by 李浩 on 2017/6/20.
+//  Copyright © 2017年 FromHell. All rights reserved.
+//
+
+#include "lru_cache.h"
---
+++ FHSqliteDemo/FHSqlite/Core/lru_cache.h
@@ -0,0 +1,18 @@
+//
+//  lru_cache.h
+//  FHSqliteDemo
+//
+//  Created by 李浩 on 2017/6/20.
+//  Copyright © 2017年 FromHell. All rights reserved.
+//
+
+#ifndef lru_cache_h
+#define lru_cache_h
+
+#include <stdio.h>
+
+typedef struct {
+    
+}_lru_cache;
+
+#endif /* lru_cache_h */
