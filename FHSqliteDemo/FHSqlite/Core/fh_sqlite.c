e6ff2b6840e3b200c96e75f8ecb29d510d05dcc1
godL <547188371@qq.com>
2017-06-22 23:02:35 +0800

    add pool db

--- FHSqliteDemo/FHSqlite/Core/fh_sqlite.c
+++ FHSqliteDemo/FHSqlite/Core/fh_sqlite.c
@@ -36,14 +36,12 @@ CF_INLINE void sqlite_db_stmt_cache_release(CFAllocatorRef allocator,const void*
     stmt = NULL;
 }
 
-
 SqliteRef sqlite_initialize(const char* sqlite_path) {
     if (sqlite_path == NULL || strlen(sqlite_path) <= 0) return NULL;
     
     SqliteRef sqlite = malloc(sizeof(_fh_sqlite));
     if (sqlite == NULL) return NULL;
-    CFDictionaryValueCallBacks value = {0,0,&sqlite_db_cache_value_release,0,0};
-    sqlite->_db_cache = CFDictionaryCreateMutable(CFAllocatorGetDefault(), 10, NULL, &value);
+    sqlite->pool = pool_create(20);
     sqlite->_sqlite_path = strdup(sqlite_path);
     sqlite_lock_initialize(sqlite);
     return sqlite;
@@ -67,7 +65,7 @@ sqlite_db sqlite_db_initialize(SqliteRef sqlite,pthread_t thread) {
 
 sqlite_db sqlite_open(SqliteRef sqlite) {
     if (sqlite == NULL) return NULL;
-    sqlite_db _db = (sqlite_db)CFDictionaryGetValue(sqlite->_db_cache, pthread_self());
+
     if (_db ) return _db;
     
     _db = sqlite_db_initialize(sqlite, pthread_self());
@@ -112,3 +110,59 @@ void sqlite_db_delloc(sqlite_db _db) {
     sqlite3_close(_db->_db);
     _db = NULL;
 }
+
+db_pool pool_create(uint32_t max_count) {
+    db_pool pool = malloc(sizeof(_db_pool));
+    if (pool == NULL) return NULL;
+    pool->_max_count = max_count;
+    pool->_pool_count = 0;
+    pool->_temp_count = 0;
+    pool->_thefirst = NULL;
+    pool->_thelast = NULL;
+    pool->_lock = dispatch_semaphore_create(1);
+    pool->_temp = NULL;
+    return pool;
+}
+
+pool_node pool_node_create(void) {
+    pool_node node = malloc(sizeof(_pool_node));
+    if (node == NULL) return node;
+    node->_used = false;
+    node->next = NULL;
+    node->prev = NULL;
+    node->_db = NULL;
+    return node;
+}
+
+void pool_node_usedify(db_pool pool) {
+    if (pool == NULL) return;
+    pool->_thefirst = node->next == NULL?node:node->next;
+    node->next = NULL;
+    pool->_thelast->next = node;
+    node->prev = pool->_thelast;
+    pool->_thelast = node;
+    node->_used = true;
+}
+
+pool_node pool_query(db_pool pool) {
+    if (pool == NULL) return NULL;
+    dispatch_semaphore_wait(pool->_lock, DISPATCH_TIME_FOREVER);
+    pool_node node = pool->_thefirst;
+    if (node == NULL) { //the first
+        node = pool_node_create();
+        node->_used = true;
+        pool->_thefirst = node;
+        pool->_thelast = node;
+        dispatch_semaphore_signal(pool->_lock);
+        return node;
+    }
+    if (node->_used == false) {
+        pool_node_usedify(pool);
+        dispatch_semaphore_signal(pool->_lock)'
+        return node;
+    }
+    
+    
+    
+}
+
--- FHSqliteDemo/FHSqlite/Core/fh_sqlite.h
+++ FHSqliteDemo/FHSqlite/Core/fh_sqlite.h
@@ -14,14 +14,6 @@
 #include <sqlite3.h>
 
 typedef struct {
-    const char* _sqlite_path;
-    pthread_mutex_t _sqlite_lock;
-    CFMutableDictionaryRef _db_cache;
-}_fh_sqlite;
-
-typedef _fh_sqlite* SqliteRef;
-
-typedef struct {
     bool _in; // Whether it is a write connection.
     pthread_t _thread;
     sqlite3* _db;
@@ -30,6 +22,36 @@ typedef struct {
 }_sqlite_db;
 
 typedef _sqlite_db* sqlite_db;
+
+typedef struct {
+    bool _used;
+    sqlite_db _db;//a sqlite_db object
+    struct _pool_node *next;
+    struct _pool_node *prev;
+    void(^poolDidFinishUsed)(struct _pool_node node);
+}_pool_node;
+
+typedef _pool_node *pool_node;
+
+typedef struct {
+    uint32_t _pool_count;
+    uint32_t _max_count;
+    uint32_t _temp_count;
+    dispatch_semaphore_t _lock;
+    pool_node _thefirst;
+    pool_node _thelast;
+    pool_node *_temp;
+}_db_pool;
+
+typedef _db_pool *db_pool;
+
+typedef struct {
+    const char* _sqlite_path;
+    pthread_mutex_t _sqlite_lock;
+    db_pool pool;
+}_fh_sqlite;
+
+typedef _fh_sqlite* SqliteRef;
 
 SqliteRef sqlite_initialize(const char* sqlite_path);
 
@@ -42,5 +64,9 @@ sqlite_db sqlite_open(SqliteRef sqlite);
 void sqlite_delloc(SqliteRef sqlite);
 
 void sqlite_db_delloc(sqlite_db _db);
+
+db_pool pool_create(uint32_t max_count);
+
+pool_node pool_query(db_pool pool);
 
 #endif /* fh_sqlite_h */
