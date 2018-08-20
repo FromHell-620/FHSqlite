7aac755c66571cf1157a47ef51cd6c1a59c7c1cf
imac <547188371@qq.com>
2018-08-20 19:00:02 +0800

    update fh_sqlite

--- FHSqliteDemo/FHSqlite/Core/fh_sqlite.c
+++ FHSqliteDemo/FHSqlite/Core/fh_sqlite.c
@@ -18,14 +18,69 @@ typedef struct fh_db {
     FHHashRef stmt_cache;
 } fh_db;
 
+typedef fh_db * FHDBRef;
+
 typedef struct fh_sqlite {
     const char *path;
     struct sqlite_configuration *congiguration;
     FHPoolRef db_pool;
 } fh_sqlite;
 
+FH_INLINE void __SqliteOpen(FHDBRef db,const char *path) {
+    if (db == NULL || db->db != NULL || strlen(path) == 0) return;
+    int result = sqlite3_open(path, &db->db);
+    if (result != SQLITE_OK) {
+        printf("sqlite open failed  %d  %s",result,sqlite3_errmsg(db->db));
+        db->db = NULL;
+        return;
+    }
+}
+
+FH_INLINE void __SqliteClose(FHDBRef db) {
+    if (db == NULL || db->db == NULL) return;
+    unsigned int count = 3;
+    FHBoolean triedFinalizingOpenStatements = false;
+    FHBoolean retry = true;
+    do {
+        count --;
+        retry = false;
+        int result = sqlite3_close(db->db);
+        if (result == SQLITE_BUSY || result == SQLITE_LOCKED) {
+            if (!triedFinalizingOpenStatements) {
+                triedFinalizingOpenStatements = true;
+                sqlite3_stmt *stmt;
+                while ((stmt = sqlite3_next_stmt(db->db, nil)) != 0) {
+                    sqlite3_finalize(stmt);
+                    retry = true;
+                }
+            }
+            
+        }else if (result != SQLITE_OK) {
+            count = 0;
+            printf("sqlite close failed  %d   %s",result,sqlite3_errmsg(db->db));
+        }
+    } while (retry && count > 0);
+}
+
 FH_INLINE void __FHDBCallbackRelease(void *value) {
-    
+    FHDBRef db = value;
+    if (db == NULL) return;
+    FHHashRelease(db->stmt_cache);
+    __SqliteClose(db);
+    free(db);
+    db = NULL;
+}
+
+FHDBRef FHDBCreate(void) {
+    FHDBRef db;
+    if ((db = calloc(1, sizeof(fh_db))) == NULL) return NULL;
+    FHHashValueCallback callback = {NULL,(FHHashReleaseCallback)__FHDBCallbackRelease,NULL};
+    db->stmt_cache = FHHashCreateWithOptions(10, &kFHCopyStringKeyCallback, &callback);
+    if (db->stmt_cache == NULL) {
+        free(db);
+        db = NULL;
+    }
+    return db;
 }
 
 FHSqliteRef FHSqliteCreate(const char *path) {
@@ -46,4 +101,13 @@ FHSqliteRef FHSqliteCreateWithOptions(const char *path,SqliteConfigurationref co
     FHPoolValueCallback callback = {NULL,__FHDBCallbackRelease};
     sqlite->db_pool = FHPoolCreateWithCallback(congifuration->pool_size, &callback, congifuration->callback);
     return sqlite;
+}
+
+void FHSqliteRelease(FHSqliteRef sqlite) {
+    if (sqlite == NULL) return;
+    sqlite->congiguration = NULL;
+    FHPoolRelease(sqlite->db_pool);
+    free((void *)sqlite->path);
+    free(sqlite);
+    sqlite = NULL;
 }
--- FHSqliteDemo/FHSqlite/Core/fh_sqlite.h
+++ FHSqliteDemo/FHSqlite/Core/fh_sqlite.h
@@ -30,59 +30,8 @@ FHSqliteRef FHSqliteCreate(const char *path);
 FH_EXTERN
 FHSqliteRef FHSqliteCreateWithOptions(const char *path,SqliteConfigurationref congifuration);
 
-//typedef struct fh_db {
-//    bool is_used;
-//    sqlite3 *db;
-//    CFMutableDictionaryRef stmt_cache;
-//} fh_db;
-//
-//typedef struct fh_pool {
-//    unsigned long used_count;
-//    unsigned long max_count;
-//    linkListNodeCallback *callback;
-//    linkList *pool;
-//    linkList *temp_pool;
-//} fh_pool;
-//
-//typedef struct fh_sqlite {
-//    const char *sqlite_path;
-//    pthread_mutex_t sqlite_lock;
-//    fh_pool *pool;
-//} fh_sqlite;
-//
-//fh_pool *pool_create(unsigned long max_count,linkListNodeCallback *callback);
-//
-//typedef struct _sqlite_db {
-//    bool _in; // Whether it is a write connection.
-//    pthread_t _thread;
-//    sqlite3* _db;
-//    const char* _sqlite_path;
-//    CFMutableDictionaryRef _stmt_cache;//stmt cache. key is sql,value is sqlite3_stmt
-//}_sqlite_db;
-//
-//typedef _sqlite_db* sqlite_db;
-//
-//typedef struct {
-//    const char* _sqlite_path;
-//    pthread_mutex_t _sqlite_lock;
-//    db_pool *pool;
-//}_fh_sqlite;
-//
-//
-//typedef _fh_sqlite* SqliteRef;
-//
-//SqliteRef sqlite_initialize(const char* sqlite_path);
-//
-//sqlite_db db_get_current(SqliteRef sqlite);
-//
-//bool db_is_in(sqlite_db db);
-//
-//sqlite_db sqlite_open(SqliteRef sqlite);
-//
-//void sqlite_delloc(SqliteRef sqlite);
-//
-//void sqlite_db_delloc(sqlite_db _db);
-
+FH_EXTERN
+void FHSqliteRelease(FHSqliteRef sqlite);
 
 
 #endif /* fh_sqlite_h */
